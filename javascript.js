b64666d104b8e99c5c016b2f4524d4c423c89cca
Julian Hinojosa <julian.hin2112@gmail.com>
2019-03-28 19:10:17 -0700

    save

--- javascript.js
+++
@@ -1,8 +0,0 @@
-function hideNav() {
-  var x = document.getElementById("topnav");
-  if (x.className === "nav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
